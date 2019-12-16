75404ed1f61dd9ba264ec982d3b0778ca781b5fe
Igor Rumiha <igorrumiha@gmail.com>
2019-12-16 10:58:25 +0100

    Regexen

--- core/src/main/scala/com/github/irumiha/pgmirror/model/generator/GeneratorModel.scala
+++ core/src/main/scala/com/github/irumiha/pgmirror/model/generator/GeneratorModel.scala
@@ -12,15 +12,15 @@ sealed abstract class Annotation(val regex: Regex)
 sealed abstract class ColumnAnnotation(override val regex: Regex) extends Annotation(regex)
 
 object ColumnAnnotation {
-  case object FilterEq      extends ColumnAnnotation("@FilterEQ\\b".r)
-  case object FilterGt      extends ColumnAnnotation("@FilterGT\\b".r)
-  case object FilterLt      extends ColumnAnnotation("@FilterLT\\b".r)
-  case object FilterGtEq    extends ColumnAnnotation("@FilterGE\\b".r)
-  case object FilterLtEq    extends ColumnAnnotation("@FilterLE\\b".r)
-  case object Find          extends ColumnAnnotation("@Find\\b".r)
-  case object FindOne       extends ColumnAnnotation("@FindOne\\b".r)
-  case object Detail        extends ColumnAnnotation("@Detail\\b".r)
-  case object Versioning    extends ColumnAnnotation("@Versioning\\b".r)
+  case object FilterEq      extends ColumnAnnotation("""@FilterEQ\b""".r)
+  case object FilterGt      extends ColumnAnnotation("""@FilterGT\b""".r)
+  case object FilterLt      extends ColumnAnnotation("""@FilterLT\b""".r)
+  case object FilterGtEq    extends ColumnAnnotation("""@FilterGE\b""".r)
+  case object FilterLtEq    extends ColumnAnnotation("""@FilterLE\b""".r)
+  case object Find          extends ColumnAnnotation("""@Find\b""".r)
+  case object FindOne       extends ColumnAnnotation("""@FindOne\b""".r)
+  case object Detail        extends ColumnAnnotation("""@Detail\b""".r)
+  case object Versioning    extends ColumnAnnotation("""@Versioning\b""".r)
 
   val values: List[ColumnAnnotation] = List[ColumnAnnotation](
     FilterEq, FilterGt, FilterLt, FilterGtEq, FilterLtEq, Find, FindOne, Detail, Versioning
@@ -31,12 +31,12 @@ object ColumnAnnotation {
 sealed abstract class TableAnnotation(override val regex: Regex) extends Annotation(regex)
 
 object TableAnnotation {
-  case object Limit        extends TableAnnotation("@Limit".r)
-  case object Offset       extends TableAnnotation("@Offset".r)
-  case object Lookup       extends TableAnnotation("@Lookup".r)
-  case object Event        extends TableAnnotation("@Event".r)
-  case object VersionCheck extends TableAnnotation("@VersionCheck".r)
-  case object History      extends TableAnnotation("@History".r)
+  case object Limit        extends TableAnnotation("""@Limit\b""".r)
+  case object Offset       extends TableAnnotation("""@Offset\b""".r)
+  case object Lookup       extends TableAnnotation("""@Lookup\b""".r)
+  case object Event        extends TableAnnotation("""@Event\b""".r)
+  case object VersionCheck extends TableAnnotation("""@VersionCheck\b""".r)
+  case object History      extends TableAnnotation("""@History\b""".r)
 
   val values: List[TableAnnotation] = List[TableAnnotation](
     Limit, Offset, Lookup, Event, VersionCheck, History
